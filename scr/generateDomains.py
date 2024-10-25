49e717f66982413a48c030567cf18dc9248e1952
TriTacLe <tritac.le@gmail.com>
2024-10-26 00:17:27 +0200

    egt

--- scr/generateDomains.py
+++ scr/generateDomains.py
@@ -1,17 +1,17 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-# API autentiseringsdetaljer
+#API autentiseringsdetaljer, ressurser som trengs
 API_TOKEN = "heytDI6dBJVcUQHV"
 API_SECRET = "ekNjmFzmJZ3Y9noTVvwavcKRo6NH3G5s09vDfeJhj9KpCOlKQsELruoTNuohFKNs"
 
-# Endepunkt for å liste opp domener
+#endepunkt for å liste opp domener
 url = "https://api.domeneshop.no/v0/domains"
 
-# Send forespørsel med autentisering
+#send forespørsel med autentisering
 response = requests.get(url, auth=HTTPBasicAuth(API_TOKEN, API_SECRET))
 
-# Sjekk responsstatus og innhold
+#check responsstatus og innhold
 if response.status_code == 200:
     print("Domeneliste hentet:")
     print(response.json())
